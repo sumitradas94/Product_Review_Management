caeb7d6fe04c11e896adc06fad647bf9fea0cba5
sumitradas94 <sumitradas94@gmail.com>
2023-02-05 21:52:21 +0530

    Management file is Updated

---
+++ Management.cs
@@ -0,0 +1,18 @@
+﻿using LINQ_ProductReviewManagement;
+
+namespace LINQ_ProductReviewManagement
+{
+    public class Management
+    {
+        public static void TopThreeByRating(List<ProductReview> productReview)
+        {
+
+            var Record = (from productreview in productReview orderby productreview.Rating descending select productreview).Take(3);
+
+            foreach (var element in Record)
+            {
+                Console.WriteLine("ProductID :{0}, UserID :{1}, Rating :{2}, Review :{3}, isLike :{4}", element.ProductID, element.UserID, element.Rating, element.Review, element.isLike);
+            }
+        }
+    }
+}
