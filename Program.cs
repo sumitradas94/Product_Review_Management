4bb5b2c08401a3dd0c5fd880e45cbcf03d3d3e27
sumitradas94 <sumitradas94@gmail.com>
2023-02-05 21:21:58 +0530

    Creating Class with Attributes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LINQ_ProductReviewManagement;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,25 @@ namespace LINQProductReviewManagement
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LINQ Product Review Management Program.");
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+            new ProductReview() { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", isLike = true},
+            new ProductReview() { ProductId = 2, UserId = 1, Rating = 4, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 3, UserId = 2, Rating = 5, Review = "Bad", isLike = false },
+            new ProductReview() { ProductId = 4, UserId = 2, Rating = 4, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 5, UserId = 3, Rating = 2, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 6, UserId = 4, Rating = 1, Review = "Bad", isLike = false },
+            new ProductReview() { ProductId = 11, UserId = 3, Rating = 4, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 7, UserId = 10, Rating = 2, Review = "Bad", isLike = false },
+            new ProductReview() { ProductId = 8, UserId = 10, Rating = 4, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 10, UserId = 7, Rating = 5, Review = "Good", isLike = true},
+            new ProductReview() { ProductId = 12, UserId = 9, Rating = 4, Review = "Bad", isLike = false },
+            new ProductReview() { ProductId = 13, UserId = 2, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 14, UserId = 5, Rating = 4, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 15, UserId = 3, Rating = 2, Review = "Bad", isLike = false },
+
+            };
         }
     }
 }
+    
