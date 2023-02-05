ea919c93c1e1151a5ce4a4653e2a559b49cc435c
sumitradas94 <sumitradas94@gmail.com>
2023-02-05 21:13:07 +0530

    Greeting Linqproductreviewmanagement

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProductReviewManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LINQ Product Review Management Program.");
+        }
+    }
+}
