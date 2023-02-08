2ef0aa79e7ef9e89ddc3d9b1be2f46e21894287a
sumitradas94 <sumitradas94@gmail.com>
2023-02-08 18:38:35 +0530

    Retreive all records from the list who’s Userid10git add Datatable.cs!

--- Datatable.cs
+++ Datatable.cs
@@ -18,20 +18,20 @@ namespace LINQ_ProductReviewManagement
             table.Columns.Add("Review");
             table.Columns.Add("isLike");
 
-            table.Rows.Add(1, 8, 4, "Good", true);
+            table.Rows.Add(5, 8, 4, "Good", true);
             table.Rows.Add(4, 2, 2, "Bad", false);
-            table.Rows.Add(33, 5, 5, "Good", true);
+            table.Rows.Add(33, 10, 5, "Good", true);
             table.Rows.Add(5, 3, 2, "Bad", false);
             table.Rows.Add(12, 1, 3, "Nice", true);
-            table.Rows.Add(6, 6, 4, "Good", true);
+            table.Rows.Add(6, 10, 4, "Good", true);
             table.Rows.Add(11, 2, 5, "Good", true);
             table.Rows.Add(9, 4, 3, "Nice", true);
             table.Rows.Add(3, 7, 4, "Good", true);
             table.Rows.Add(2, 10, 1, "Bad", false);
             table.Rows.Add(5, 3, 5, "Good", true);
             table.Rows.Add(1, 1, 4, "Good", true);
-            table.Rows.Add(4, 2, 2, "Bad", false);
-            table.Rows.Add(3, 5, 5, "Good", true);
+            table.Rows.Add(4, 10, 2, "Bad", false);
+            table.Rows.Add(3, 9, 5, "Good", true);
             table.Rows.Add(5, 3, 2, "Bad", false);
             table.Rows.Add(1, 1, 3, "Nice", true);
             table.Rows.Add(6, 6, 4, "Good", true);
@@ -40,36 +40,30 @@ namespace LINQ_ProductReviewManagement
             table.Rows.Add(3, 7, 4, "Good", true);
             table.Rows.Add(2, 10, 1, "Bad", false);
             table.Rows.Add(5, 3, 5, "Good", true);
-            table.Rows.Add(9, 5, 4, "Good", true);
+            table.Rows.Add(9, 10, 4, "Good", true);
             table.Rows.Add(1, 1, 4, "Good", true);
-            table.Rows.Add(4, 2, 2, "Bad", false);
+            table.Rows.Add(5, 2, 2, "Bad", false);
 
             Display(table);
         }
         public static void Display(DataTable data)
         {
-            //var Records = from pd in data.AsEnumerable()
-            //        select new
-            //        {
-            //            productid = pd.Field<string>("ProductID"),
-            //            userid = pd.Field<string>("UserID"),
-            //            rating = pd.Field<string>("Rating"),
-            //            review = pd.Field<string>("Review"),
-            //            islike = pd.Field<string>("isLike")
-            //        };
-
-            var Records = data.AsEnumerable().GroupBy(x => x.Field<int>("ProductID"))
-                .Select(x => new
-                {
-                    ProductID = x.Key,
-                    Rating = x.Average(x => x.Field<int>("Rating"))
-                }).ToList();
+            var Records = from pd in data.AsEnumerable()
+                          select new
+                          {
+                              productid = pd.Field<string>("ProductID"),
+                              userid = pd.Field<string>("UserID"),
+                              rating = pd.Field<string>("Rating"),
+                              review = pd.Field<string>("Review"),
+                              islike = pd.Field<string>("isLike")
+                          };
 
             foreach (var element in Records)
             {
-
-                Console.WriteLine(element);
-
+                if (element.userid == "10")
+                {
+                    Console.WriteLine(element);
+                }
             }
         }
     }
